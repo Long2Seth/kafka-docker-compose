5682be9a54a3bc936a3fa53990d21a5672b2b25b
Long2Seth <stevkhat121212@gmail.com>
2024-09-02 09:01:20 +0700

    commit

---
+++ src/main/java/com/example/kafkadockercompose/KafkaDockerComposeApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafkadockercompose;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaDockerComposeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaDockerComposeApplication.class, args);
+	}
+
+}
